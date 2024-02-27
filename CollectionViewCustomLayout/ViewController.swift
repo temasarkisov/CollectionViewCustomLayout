ae07dcf2e9633cb45d121f908dbee940dc907447
temasarkisov <a.sarkisov.s@yandex.ru>
2024-02-27 17:11:40 +0200

    Initial Commit

---
+++ CollectionViewCustomLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewCustomLayout
+//
+//  Created by Артем Саркисов on 27.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
